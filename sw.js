792797d74ebd30d79610a062e1587c8a31ce6690
Krishna dhar <krishnadhar002@gmail.com>
2026-09-07 12:20:18 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'gold-calc-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
